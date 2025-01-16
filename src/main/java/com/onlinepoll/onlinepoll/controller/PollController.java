f53ac593a4b1b5db2d15c0ad5ed62fb4fd28d348
Mangesh98 <mangeshraje55555@gmail.com>
2025-01-16 19:38:21 +0530

    getAllPolls & getPollById Endpoint implemented

--- src/main/java/com/onlinepoll/onlinepoll/controller/PollController.java
+++ src/main/java/com/onlinepoll/onlinepoll/controller/PollController.java
@@ -2,10 +2,10 @@ package com.onlinepoll.onlinepoll.controller;
 
 import com.onlinepoll.onlinepoll.model.Poll;
 import com.onlinepoll.onlinepoll.services.PollService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/polls")
@@ -18,8 +18,18 @@ public class PollController {
 
     @PostMapping("/")
     public Poll createPoll(@RequestBody Poll poll) {
-        System.out.println("Create Poll" + poll);
         return pollService.createPoll(poll);
+    }
+
+    @GetMapping("/")
+    public List<Poll> getAllPolls() {
+        return pollService.getAllPolls();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Poll> getPollById(@PathVariable Long id) {
+        return pollService.getPollById(id).map(ResponseEntity::ok).
+                orElse(ResponseEntity.notFound().build());
     }
 
 }
--- src/main/java/com/onlinepoll/onlinepoll/services/PollService.java
+++ src/main/java/com/onlinepoll/onlinepoll/services/PollService.java
@@ -2,7 +2,11 @@ package com.onlinepoll.onlinepoll.services;
 
 import com.onlinepoll.onlinepoll.model.Poll;
 import com.onlinepoll.onlinepoll.repositories.PollRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PollService {
@@ -14,5 +18,14 @@ public class PollService {
 
     public Poll createPoll(Poll poll) {
         return pollRepository.save(poll);
+    }
+
+    public List<Poll> getAllPolls() {
+        return pollRepository.findAll();
+    }
+
+
+    public Optional<Poll> getPollById(Long id) {
+        return pollRepository.findById(id);
     }
 }
