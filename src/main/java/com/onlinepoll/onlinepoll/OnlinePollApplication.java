cfadfc416f0ea15808bf6f00a4fb5b5317f1c178
Mangesh98 <mangeshraje55555@gmail.com>
2025-01-16 11:35:01 +0530

    init

---
+++ src/main/java/com/onlinepoll/onlinepoll/OnlinePollApplication.java
@@ -0,0 +1,13 @@
+package com.onlinepoll.onlinepoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinePollApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onlinepoll/onlinepoll/OnlinePollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinepoll.onlinepoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
